a7fa27a7ea289b34a221a601df78fd6e51344787
Christian Buado <worldakai@gmail.com>
2018-06-02 16:29:13 -0700

    another type of array

---
+++ variables/arrays_associative.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<?php 
+
+		$names = array("first_name" => 'Edwin', "last_name" => 'Diaz');
+
+		echo $names['first_name'] ." ". $names ['last_name'];
+
+	?>
+	
+</body>
+</html>
