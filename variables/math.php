e62619086344e0d50b6716c2922223c0b7d577c1
Christian Buado <worldakai@gmail.com>
2018-06-02 15:27:55 -0700

    math in php

---
+++ variables/math.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+	<?php 
+		echo "32 + 12 = ";
+		echo 32 + 12;
+		echo "<br>";
+		
+		echo "32 - 12 = ";
+		echo 32 - 12;
+		echo "<br>";
+		
+		echo "32 * 12 = ";
+		echo 32 * 12;
+		echo "<br>";
+
+		echo "32 / 12 = ";
+		echo 32 / 12;
+		echo "<br>";
+
+
+		$number1 = 12;
+		$number2 = 30;
+
+		echo $number1 - $number2; 
+
+	?>
+
+
+
+
+
+</body>
+</html>
