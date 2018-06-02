cd823b7ae68d023990147dd55cb01bac13c19b68
Christian Buado <worldakai@gmail.com>
2018-06-02 15:29:04 -0700

    array in php

---
+++ variables/array.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		
+		$numberList = array(267, 154, 345, "417" , 125 , "<h1>Hello</>");
+
+		echo $numberList[3];
+
+
+
+
+
+	?>
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
