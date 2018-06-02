5dda74ec7c875fef30c2f56f7d75bb35df87c393
Christian Buado <worldakai@gmail.com>
2018-06-02 02:20:51 -0700

    The file has been added

---
+++ First-Step/comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+<?php 
+
+// This code below displays Hello Student
+  echo "Hello Student";
+			
+?>
+	
+	
+
+	
+	
+</body>
+</html>
