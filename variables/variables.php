f5309fc78c3e75fa06faa104d37afa2a27e5c722
Christian Buado <worldakai@gmail.com>
2018-06-02 15:25:04 -0700

    variables in php

---
+++ variables/variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
+<?php 
+	$name = "Edwin";
+	$number = 100;
+
+		// the dot(.) is a symbol that concatinate a two data in a one.
+	echo $name . " ". $number;
+		// you can put an html tags in a php string data type..
+	echo $htmltags = "<h1>I am h1 tags</>";
+
+
+?>
+
+
+
+</body>
+</html>
