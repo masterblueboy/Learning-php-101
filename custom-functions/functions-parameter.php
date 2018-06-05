6c144bea04c402a0c45754011a85775da7cae9de
Christian Buado <worldakai@gmail.com>
2018-06-04 19:30:02 -0700

    functions with parameters

---
+++ custom-functions/functions-parameter.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Functions Parameter</title>
+</head>
+<body>
+	
+	<?php 
+		/*
+		function greeting($message){
+
+			echo "<h1> $message</h1>"; )
+
+
+		}
+
+		greeting("Hello Student");*/	
+
+
+
+		function addNumbers($number1, $number2){
+
+			$sum = $number1 + $number2;
+
+				echo "<h1>$sum</h1>";
+		}
+			addNumbers(48, 48);
+
+
+
+	?>
+</body>
+</html>
